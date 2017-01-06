7546cc0326243b18bc30a2280548e593aefa63a9
Michael Porter <firsttogo98@gmail.com>
2017-01-06 09:53:09 -0600

    Update settings.py

--- Unleashed/settings.py
+++ Unleashed/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-z87$(p-q&z((srl+0c9*eqzh+ihej0294zb(4&fid3+rm0(s*'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
